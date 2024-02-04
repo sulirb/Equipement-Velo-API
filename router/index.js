b08f713fad566443da38e47288c9ce573d176504
sulirb <121161440+sulirb@users.noreply.github.com>
2024-02-04 19:43:01 +0100

    cached

--- router/index.js
+++ router/index.js
@@ -6,7 +6,7 @@ router.use("/articles", require("../api/articles/index.js"));
 router.use("/articles/latest", require("../api/articles/index.js"));
 router.use("/article/:id", require("../api/articles/[id]/index.js"));
 router.use("/auth/login", require("../api/auth/login.js"));
-router.use("/auth/signup", require("../api/auth/signup.js"));
+/*router.use("/auth/signup", require("../api/auth/signup.js"));*/
 router.use("/images", require("../api/images/index.js"));
 
 module.exports = router;
