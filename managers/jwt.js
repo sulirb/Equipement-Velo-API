ddcb5cd45a34a7df6f86c3d8df540d46613dd530
sulirb <121161440+sulirb@users.noreply.github.com>
2024-07-06 17:36:52 +0200

    jwt testing end

--- managers/jwt.js
+++ managers/jwt.js
@@ -1,8 +1,8 @@
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET, JWT_EXPIRE } = require("./env.js");
 
-/* (!JWT_EXPIRE) throw new Error("JWT_EXPIRE must be set in .env");
-if (!JWT_SECRET) throw new Error("JWT_SECRET must be set in .env.local");*/
+if (!JWT_EXPIRE) throw new Error("JWT_EXPIRE must be set in .env");
+if (!JWT_SECRET) throw new Error("JWT_SECRET must be set in .env.local");
 
 const verify = (token) => jwt.verify(token, JWT_SECRET);
 const sign = (data) => jwt.sign(data, JWT_SECRET, { expiresIn: JWT_EXPIRE });
