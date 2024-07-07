c876a97a979cbf794ce499f3613185a6d0a50cea
sulirb <121161440+sulirb@users.noreply.github.com>
2024-07-07 14:06:23 +0200

    jwt for adaptable

--- managers/jwt.js
+++ managers/jwt.js
@@ -1,10 +1,10 @@
+require("dotenv").config();
 const jwt = require("jsonwebtoken");
-const { JWT_SECRET, JWT_EXPIRE } = require("./env.js");
 
-/*if (!JWT_EXPIRE) throw new Error("JWT_EXPIRE must be set in .env");
-if (!JWT_SECRET) throw new Error("JWT_SECRET must be set in .env");*/
+const sectok = process.env.JWT_SECRET;
+const exptok = process.env.JWT_EXPIRE;
 
-const verify = (token) => jwt.verify(token, JWT_SECRET);
-const sign = (data) => jwt.sign(data, JWT_SECRET, { expiresIn: JWT_EXPIRE });
+const verify = (token) => jwt.verify(token, sectok);
+const sign = (data) => jwt.sign(data, sectok, { expiresIn: exptok });
 
 module.exports = { verify, sign };
