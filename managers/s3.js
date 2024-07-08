972d535b9dd52ea72b8463321198da47042357d4
sulirb <121161440+sulirb@users.noreply.github.com>
2024-07-08 16:19:09 +0200

    s3 on vercel

--- managers/s3.js
+++ managers/s3.js
@@ -4,8 +4,8 @@ const fs = require("fs");
 
 const bucketName = process.env.AWS_BUCKET_NAME;
 const region = process.env.AWS_BUCKET_REGION;
-const accessKeyId = process.env.AWS_ACCESS_KEY;
-const secretAccessKey = process.env.AWS_SECRET_KEY;
+const accessKeyId = process.env.AWS_ACCESSS_KEY;
+const secretAccessKey = process.env.AWS_SECRETE_KEY;
 
 const s3 = new S3({
   region,
